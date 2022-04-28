4ea32241595e006643e3f97be4eaf8cb6af970fa
Мария <ppolyschuk@gmail.com>
2022-04-28 19:59:16 +0300

    Подключение api, добавление фильмов, таблица предложений

--- data/films.py
+++ data/films.py
@@ -9,10 +9,8 @@ class Films(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     kinopoisk_id = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
-    festival_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("festivals.id")) # доделать когда появится таблица с фестивалями
+    festival_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("festivals.id"))
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    is_approved = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
+
     festival = orm.relation('Festival')
-    user = orm.relation('User')
